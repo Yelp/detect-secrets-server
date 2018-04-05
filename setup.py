3141adf56a00852d09f67699953afd5c143e54ea
Kevin Hock <khock@yelp.com>
2018-04-05 13:41:33 -0700

    Add first attempt at proper setup.py files, + add detect-secrets to requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import find_packages
 from setuptools import setup
 
+
+VERSION = '0.1.0'
+
 setup(
-    name='detect_secrets',
-    description='Tool for detecting secrets in the codebase',
-    author='Aaron Loo',
+    name='detect_secrets_server',
     packages=find_packages(exclude=(['test*', 'tmp*'])),
+    version=VERSION,
+    description='Tool for setting up a detect-secrets server',
+    long_description="Check out detect-secrets-server on `GitHub <https://github.com/Yelp/detect-secrets-server>`_!",
+    license="Copyright Yelp, Inc. 2018",
+    author='Aaron Loo',
+    author_email='aaronloo@yelp.com',
+    url='https://github.com/Yelp/detect-secrets-server',
+    download_url='https://github.com/Yelp/detect-secrets-server/archive/{}.tar.gz'.format(VERSION),
+    keywords=['secret-management', 'pre-commit', 'security', 'entropy-checks'],
     install_requires=[
+        'detect-secrets',
         'enum34',
         'future',
         'pyyaml',
@@ -14,9 +25,18 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'detect-secrets = detect_secrets.main:main',
-            'detect-secrets-hook = detect_secrets.pre_commit_hook:main',
+            'detect-secrets-server = detect_secrets_server.__main__:main',
         ],
     },
-    version='0.7.0',
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+    ]
 )
