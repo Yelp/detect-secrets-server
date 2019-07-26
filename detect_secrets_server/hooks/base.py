6c0843c7d78f11679bfb072f99bdd5a417e4b818
KevinHock <kevin.hock.opentoall@gmail.com>
2019-07-26 14:45:04 -0700

    :mortar_board: `eg.` -> `e.g.`

--- detect_secrets_server/hooks/base.py
+++ detect_secrets_server/hooks/base.py
@@ -5,7 +5,7 @@ from abc import abstractmethod
 class BaseHook(object):  # pragma: no cover
     """This is an abstract class to define Hooks API. A hook is an alerting system
     that allows you connect your server scanning results to your larger ecosystem
-    (eg. email alerts, IRC pings...)
+    (e.g. email alerts, IRC pings...)
     """
 
     __metaclass__ = ABCMeta
