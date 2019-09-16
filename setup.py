c64f35b76d7fabae63e44f7597ba7c601f4ebc42
Kevin Hock <khock@yelp.com>
2019-09-16 12:22:22 -0700

    :fist::pushpin: To v0.12.6 of detect-secrets
    This was done via
    ```
    vim requirements* setup.py
    :bufdo %s/0.12.2/0.12.6/ge | update
    ```

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         'entropy-checks'
     ],
     install_requires=[
-        'detect-secrets>=0.12.2',
+        'detect-secrets>=0.12.6',
         'pyyaml',
         'unidiff',
         'python-crontab',
